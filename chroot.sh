c3d324be9863e96ab8aa07970e0d4a68c23f3572
Mike Hudgins <mchudgins@dstsystems.com>
2016-10-13 13:52:12 -0500

    updated chroot.sh to auto-magically mount and chroot

--- chroot.sh
+++ chroot.sh
@@ -1,8 +1,22 @@
 # run these commands as part of chroot
+echo "args ${#}"
+if test ${#} -eq 0 ; then
+  echo "Usage:  `basename $0` <mount point>"
+  exit 1
+fi
 
-cd "to mount point"
+MNT=${1}
+if test ! -d ${MNT} ; then
+  echo "`basename $0` -- ${MNT} is not a directory"
+  exit 2
+fi
+
+cd ${MNT}
 mount -t proc proc proc/
 mount -t sysfs sys sys/
 mount -o bind /dev dev/
 cp /etc/resolv.conf etc/
-chroot "mount point"
+chroot ${MNT}
+umount ${MNT}/proc
+umount ${MNT}/sys
+umount ${MNT}/dev
